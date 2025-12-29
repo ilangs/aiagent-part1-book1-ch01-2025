87f0efad17b2e58045bc03b1f12c7bf362f59a59
ilangs <ilangs2011@gmail.com>
2025-12-29 11:51:55 +0900

    second commit

---
+++ ch01ex03.py
@@ -0,0 +1,14 @@
+numbers = [1, 2, 3, 4, 5]
+
+total = 0
+for n in numbers:
+    total += n
+
+print("리스트 합계:", total)
+
+
+import numpy as np
+
+numbers = np.array([1, 2, 3, 4, 5])
+print("NumPy 합계:", np.sum(numbers))
+
